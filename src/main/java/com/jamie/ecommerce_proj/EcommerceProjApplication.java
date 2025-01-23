eb2bc24fae0f7431deaad0ac6e7583b337980009
Jamie Doyle <jamedoyle@Jamies-MacBook-Air-2.local>
2025-01-23 18:37:51 +0000

    Initial commit

---
+++ src/main/java/com/jamie/ecommerce_proj/EcommerceProjApplication.java
@@ -0,0 +1,15 @@
+package com.jamie.ecommerce_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class EcommerceProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceProjApplication.class, args);
+	}
+
+}
